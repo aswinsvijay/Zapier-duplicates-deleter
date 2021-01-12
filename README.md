4903b23cf645255dffe1294240dc1de78030e88b
Aswin S Vijay <aswinsvijay03052000@gmail.com>
2021-01-12 19:00:41 +0530

    implemented duplicate deletion

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # JTF-CCX-Project
 Helper bot for Zapier to delete duplicates.  
 Add the bot token to the ```.env``` file before running the code.  
-
+The bot uses email present in the message as primary key to find duplicates and delete them. It is required that there should be at least one whitespace before and after the email, and the ```@``` symbol must not be present anywhere in the message other than in the email.  
--- deleterbot.py
+++ deleterbot.py
@@ -17,4 +17,22 @@ async def on_message(message : discord.Message):
     if message.author == client.user:           #if message is from this bot itself, ignore
         return
 
+    message.content = message.content.split()   #split the content by whitespaces
+
+    #finding the email from the message
+    for i in message.content:
+        if '@' in i:
+            mail = i
+            break
+
+    #history() gives messages in reverse chronological order
+    #flag is used to skip the first occurence(the latest message)
+    flag = False
+    async for i in message.channel.history():
+        if mail in i.content:                   #if mail is present in the message
+            if not flag:                        #set flag to True the first time
+                flag = True
+            else:                               #delete when found the next time
+                await i.delete()
+
 client.run(TOKEN)
